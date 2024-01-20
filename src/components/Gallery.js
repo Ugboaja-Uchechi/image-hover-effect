2ccbf9d5bc2e825d5180d139f5c0eac3a1eeabeb
Ugboaja-Uchechi <stephanieuchechi754@gmail.com>
2024-01-20 20:59:55 +0100

    Complete image hover effect

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import cn from "classnames"
 import "../styles/gallery.scss"
+import { CursorContext } from '../CustomCursor/Cursormanager';
 
 const images = [
   "https://images.unsplash.com/photo-1704809938308-8336b66d3fb5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -9,15 +10,54 @@ const images = [
 ];
 
 export const GalleryItem = ({ src }) => {
+  const ref = useRef(null);
+  const mouseContext = useContext(CursorContext);
+
+  const [clipMaskRadius, setClipMaskRadius] = useState(0);
+  const [clipMask, setClipMask] = useState({ x: 0, y: 0 })
   const [reveal, setReveal] = useState(false);
 
   useEffect(() => {
-    setTimeout(() =>{
+    setTimeout(() => {
       setReveal(true);
     }, 100)
-  },[])
+  }, []);
+
+  useEffect(() => {
+    function getCoordinates(mouse) {
+      const imagePosition = {
+        posX: ref.current.offsetLeft,
+        posY: ref.current.offsetTop,
+      };
+
+      const posX = mouse.pageX - imagePosition.posX;
+      const posY = mouse.pageY - imagePosition.posY;
+
+      setClipMask({
+        x: (posX / ref.current.clientWidth) * 100,
+        y: (posY / ref.current.clientHeight) * 100,
+      });
+    }
+
+    ref.current.addEventListener("mousemove", (mouse) => {
+      window.requestAnimationFrame(() => {
+        getCoordinates(mouse);
+      });
+    });
+  }, []);
+
   return (
-    <div className={cn('gallery-item-wrapper', { "is-reveal": reveal })}>
+    <div className={cn('gallery-item-wrapper', { "is-reveal": reveal })}
+      ref={ref}
+      onMouseEnter={() => {
+        setClipMaskRadius(25);
+        mouseContext.setSize("hide");
+      }}
+      onMouseLeave={() => {
+        setClipMaskRadius(0);
+        mouseContext.setSize("small");
+      }}
+    >
       <div className='gallery-item'>
         <div
           className='gallery-item-image sepia'
@@ -26,7 +66,10 @@ export const GalleryItem = ({ src }) => {
         </div>
         <div
           className='gallery-item-image masked'
-          style={{ backgroundImage: `url(${src})` }}
+          style={{
+            backgroundImage: `url(${src})`,
+            clipPath: `circle(${clipMaskRadius}% at ${clipMask.x}% ${clipMask.y}%)`,
+          }}
         >
         </div>
       </div>
